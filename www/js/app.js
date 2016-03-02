d19d17d14245752ccfefd37354752cd881ccd953
Mark Ryan <mark.ryan25@gmail.com>
2016-03-02 23:53:59 +0200

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('blank', ['ionic'])
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    $stateProvider
+
+    .state('home', {
+        url: '/home',
+        templateUrl: 'templates/layout.html',
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/home');
+
+});
